80fd39e2267eb4fcb0a14ca674c278a5bc222d78
Dalton Ehret <ehretd@pella.com>
2025-05-06 16:17:52 -0500

    fix(snippets): Added Priority to my Snippets to overwrite friendly-snippets 5

--- lua/ehretd/lazy/snippets.lua
+++ lua/ehretd/lazy/snippets.lua
@@ -18,21 +18,63 @@ return {
         vim.keymap.set({"i", "s"}, "<C-p>", function() ls.jump(1) end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-o>", function() ls.jump(-1) end, {silent = true})
 
-        ls.add_snippets("all", {
+        ls.add_snippets("javascript", {
             ls.parser.parse_snippet(
                 {
                     trig = "here",
                     name = "Insert console.log('here')",
+                    priority = 2000,
                 },
                 "console.log('here');"
             ),
             ls.parser.parse_snippet(
                 {
+                    trig = "log",
+                    name = "Insert console.log()",
+                    priority = 2000,
+                },
+                "console.log($0);"
+            ),
+            ls.parser.parse_snippet(
+                {
                     trig = "test",
                     name = "Insert test function",
+                    priority = 2000,
                 },
                 "test('$1', () => {\n\t$0\n});"
             ),
-        }, { key = "all" })
+            ls.parser.parse_snippet(
+                {
+                    trig = "acaf",
+                    name = "Async Arrow Function",
+                    priority = 2000,
+                },
+                "const $1 = async ($2) => {\n\t$0\n};"
+            ),
+             ls.parser.parse_snippet(
+                {
+                    trig = "caf",
+                    name = "Arrow Function",
+                    priority = 2000,
+                },
+                "const $1 = ($2) => {\n\t$0\n};"
+            ),
+            ls.parser.parse_snippet(
+                {
+                    trig = "ae",
+                    name = "Inserts afterEach() block",
+                    priority = 2000,
+                },
+                "afterEach(() => {\n\t$0\n});"
+            ),
+            ls.parser.parse_snippet(
+                {
+                    trig = "be",
+                    name = "Inserts beforeEach() block",
+                    priority = 2000,
+                },
+                "beforeEach(() => {\n\t$0\n});"
+            ),
+        }, { key = "javascript" });
     end,
 }
