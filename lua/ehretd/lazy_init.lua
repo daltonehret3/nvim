5cfb17714db98609d48a204efabc11991bfe0c80
daltonehret3 <daltonehret@gmail.com>
2025-06-25 12:46:06 -0500

    fix(linux): Remove Hererocks from install process

--- lua/ehretd/lazy_init.lua
+++ lua/ehretd/lazy_init.lua
@@ -11,7 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ 
+require("lazy").setup({
 	spec = "ehretd.lazy",
-	change_detection = { notify = false } 
+    rocks = {
+        enabled = false,
+    },
+    change_detection = { notify = false }
 })
