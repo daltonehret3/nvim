3947e4df382326b8f037a07e04692147d937fc53
Dalton Ehret <ehretd@pella.com>
2025-07-07 16:59:05 -0500

    chore(lsp): Added Border to LSP Hover box

--- lua/ehretd/init.lua
+++ lua/ehretd/init.lua
@@ -12,8 +12,19 @@ autocmd('LspAttach', {
 	callback = function(e)
 		local opts = { buffer = e.buf }
 
+        local border = {
+            { '┌', 'FloatBorder' },
+            { '─', 'FloatBorder' },
+            { '┐', 'FloatBorder' },
+            { '│', 'FloatBorder' },
+            { '┘', 'FloatBorder' },
+            { '─', 'FloatBorder' },
+            { '└', 'FloatBorder' },
+            { '│', 'FloatBorder' },
+        }
+
 		vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-		vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+		vim.keymap.set("n", "K", function() vim.lsp.buf.hover({border=border}) end, opts)
 		vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 		vim.keymap.set("n", "<leader>vd", function() vim.diagnostics.openfloat() end, opts)
 		vim.keymap.set("n", "[d", function() vim.diagnostics.goto_next() end, opts)
