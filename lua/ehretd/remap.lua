61f7a5a8e04e04a84e452c0a1571d2d91c7501b5
Dalton Ehret <ehretd@pella.com>
2025-08-01 14:27:38 -0500

    feat(navigation): Clean Navigation for Cursor Jumping

--- lua/ehretd/remap.lua
+++ lua/ehretd/remap.lua
@@ -8,3 +8,8 @@ vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
 
+vim.keymap.set({"n", "v"}, "<C-d>", "<C-d>zz")
+vim.keymap.set({"n", "v"}, "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
