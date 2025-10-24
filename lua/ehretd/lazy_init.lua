5fabe0ea4da5b6848a35d402bc9fa4d55c4960f4
Dalton Ehret <ehretd@pella.com>
2025-10-24 12:01:32 -0500

    fix(icons): Show Vim Icons

--- lua/ehretd/lazy_init.lua
+++ lua/ehretd/lazy_init.lua
@@ -1,20 +1,38 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = "ehretd.lazy",
+    spec = "ehretd.lazy",
     rocks = {
         enabled = false,
     },
-    change_detection = { notify = false }
+    change_detection = { notify = false },
+    ui = {
+        border = "rounded",
+        icons = {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    },
 })
