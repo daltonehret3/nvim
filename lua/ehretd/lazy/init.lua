f7d6d1b452c03f6f0c966a4a43b2cf5962511d0b
Dalton Ehret <ehretd@pella.com>
2025-11-05 10:03:28 -0600

    feat(lualine): Lua Line Status Bar

--- lua/ehretd/lazy/init.lua
+++ lua/ehretd/lazy/init.lua
@@ -9,6 +9,38 @@ return {
     "folke/zen-mode.nvim",
     "nvim-tree/nvim-web-devicons",
     {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons", "folke/tokyonight.nvim" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "tokyonight",
+                    icons_enabled = true,
+                    -- always_divide_middle = true,
+                    always_show_tabline = true,
+                    section_separators = { left = '', right = '' },
+                    component_separators = { left = '', right = '' },
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {{'datetime', style = '%m-%d-%y %H:%M'}, 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {'filename'},
+                    lualine_x = {'location'},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+            })
+        end,
+    },
+    {
         "nvim-neotest/neotest",
         commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c",
         -- lazy = true,
