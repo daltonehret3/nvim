c08449e1cb09ff8a11e8ecfdcbbd39cd00a3ff79
Dalton Ehret <ehretd@pella.com>
2025-12-03 09:35:58 -0600

    chore(vertical-line): Removed Vertical Line on Column 120 (may bring back eventually)

--- lua/ehretd/lazy/colors.lua
+++ lua/ehretd/lazy/colors.lua
@@ -4,7 +4,7 @@ function ColorMyPencils(color)
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.opt.colorcolumn = "120"
+    vim.opt.colorcolumn = "0"
 end
 
 return {
