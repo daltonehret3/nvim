28f677ac133787db140d2b80520665a3002cac5c
Dalton Ehret <ehretd@pella.com>
2026-01-13 11:55:15 -0600

    fix(jumping): Fix Up/Down Jump to not be Glitchy

--- lua/ehretd/set.lua
+++ lua/ehretd/set.lua
@@ -20,7 +20,7 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 25
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
